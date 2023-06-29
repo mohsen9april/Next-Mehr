b355e3b80792b5a9fd1a6a930ffee11336c2045e
Mohsen9april <Mohsen9april@gmail.com>
2023-06-29 10:54:05 +0330

    first commit

---
+++ app/[lng]/page.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { Trans } from "react-i18next/TransWithoutContext";
+import { languages, fallbackLng } from "../i18n/settings";
+import { useTranslation } from "../i18n";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+export async function generateMetadata({ params: { lng } }) {
+  const { t } = await useTranslation(lng);
+  return { title: t("h1") };
+}
+
+export default async function Page({ params: { lng } }) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng;
+  const { t } = await useTranslation(lng);
+
+  return (
+    <>
+      <main>
+        <Header heading={t("h1")} />
+        <h2>
+          <Trans t={t} i18nKey="welcome">
+            Welcome to Next.js v13 <small>appDir</small> and i18next
+          </Trans>
+        </h2>
+
+        <hr style={{ marginTop: 20, width: "90%" }} />
+        <div>
+          <Link href={`/${lng}/second-page`}>
+            <button type="button">{t("to-second-page")}</button>
+          </Link>
+          <Link href={`/${lng}/client-page`}>
+            <button type="button">{t("to-client-page")}</button>
+          </Link>
+        </div>
+      </main>
+      <Footer lng={lng} />
+    </>
+  );
+}
